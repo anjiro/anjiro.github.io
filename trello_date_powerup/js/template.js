079fac0b2f317fc1f36268eb5c33a8462c6898af
Daniel Ashbrook <daniel.ashbrook@rit.edu>
2017-03-06 22:00:54 -0500

    test for null

--- trello_date_powerup/js/template.js
+++ trello_date_powerup/js/template.js
@@ -3,6 +3,8 @@ var getBadges = function(t)
   return t.card('due').get('due').then(
 		function(cardDue)
 		{
+			if(!cardDue)
+				return [];
 			var dueDate = new Date(Date.parse(cardDue));
 			var daysLeft = Math.floor((dueDate - Date.now())/(3600*24*1000)) + 1;
 			return [{text: daysLeft, color: 'red'}];
